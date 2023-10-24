604bfae7e6cb50cb9b281012f5bef425b95f1871
Jens de Bruijn <j.a.debruijn@outlook.com>
2023-10-24 09:22:57 +0200

    remove pyplantfate as requirement

--- cwatm/hydrological_modules/plantFATE.py
+++ cwatm/hydrological_modules/plantFATE.py
@@ -1,9 +1,104 @@
+from datetime import datetime
+
 import numpy as np
-import pyplantFATE
+import pandas as pd
+
+from plantFATE import Simulator as sim
+from plantFATE import Clim
+
+class PlantFATERunner:
+    saveOutputs = True
+    environment = pd.DataFrame(columns=['date', 'tair', 'ppfd_max', 'ppfd', 'vpd', 'elv', 'co2', 'swp', "type"])
+    emergentProps = pd.DataFrame()
+    speciesProps = pd.DataFrame()
+
+    def __init__(self, param_file):
+        self.plantFATE_model = sim(param_file)
+        # self.soil_file = pd.read_csv(soil_file)
+
+    def init(self, tstart, tend):
+        self.plantFATE_model.init(tstart, tend)
+
+    def init(self, tstart, temp0, soil_water_potential, vpd_0, par_0):
+        photosynthetically_active_radiation = par_0 * 2.15
+        newclim = Clim()
+        newclim.tc = temp0
+        newclim.ppfd_max = photosynthetically_active_radiation * 4
+        newclim.ppfd = photosynthetically_active_radiation
+        newclim.vpd = vpd_0 * 1000
+        newclim.swp = soil_water_potential
+
+        datestart = tstart
+        datediff = datestart - datetime(datestart.year, 1, 1)
+        tstart = datestart.year + datediff.days / 365
+        self.plantFATE_model.init(tstart, newclim)
+
+    def runstep(self, soil_water_potential, vapour_pressure_deficit, photosynthetically_active_radiation,
+                temperature):
+        photosynthetically_active_radiation = photosynthetically_active_radiation * 2.15
+        self.plantFATE_model.update_environment(
+            temperature,
+            photosynthetically_active_radiation * 4,
+            photosynthetically_active_radiation,
+            vapour_pressure_deficit * 1000,
+            np.nan,
+            np.nan,
+            soil_water_potential
+        )
+        self.plantFATE_model.simulate_step()
+
+        self.saveEnvironment()
+        self.saveEmergentProps()
+
+        trans = self.plantFATE_model.props.trans
+        # return evapotranspiration, soil_specific_depletion_1, soil_specific_depletion_2, soil_specific_depletion_3
+        return trans, 0, 0, 0
+
+    def saveEnvironment(self):
+        e = pd.DataFrame({
+            'date': [self.plantFATE_model.E.tcurrent, self.plantFATE_model.E.tcurrent],
+            'tair': [self.plantFATE_model.E.weightedAveClim.tc, self.plantFATE_model.E.currentClim.tc],
+            'ppfd_max': [self.plantFATE_model.E.weightedAveClim.ppfd_max, self.plantFATE_model.E.currentClim.ppfd_max],
+            'ppfd': [self.plantFATE_model.E.weightedAveClim.ppfd, self.plantFATE_model.E.currentClim.ppfd],
+            'vpd': [self.plantFATE_model.E.weightedAveClim.vpd, self.plantFATE_model.E.currentClim.vpd],
+            'elv': [self.plantFATE_model.E.weightedAveClim.elv, self.plantFATE_model.E.currentClim.elv],
+            'co2': [self.plantFATE_model.E.weightedAveClim.co2, self.plantFATE_model.E.currentClim.co2],
+            'swp': [self.plantFATE_model.E.weightedAveClim.swp, self.plantFATE_model.E.currentClim.swp],
+            'type': ['WeightedAverage', 'Instantaneous']
+        })
+
+        self.environment = pd.concat([self.environment, e])
+
+    def saveEmergentProps(self):
+        e = pd.DataFrame({
+            'date': [self.plantFATE_model.tcurrent],
+            'trans': [self.plantFATE_model.props.trans],
+            'gs': [self.plantFATE_model.props.gs],
+            'gpp': [self.plantFATE_model.props.gpp],
+            'lai': [self.plantFATE_model.props.lai],
+            'npp': [self.plantFATE_model.props.npp],
+            'cc_est': [self.plantFATE_model.props.cc_est],
+            'croot_mass': [self.plantFATE_model.props.croot_mass],
+            'froot_mass': [self.plantFATE_model.props.froot_mass],
+            'lai_vert': [self.plantFATE_model.props.lai_vert],
+            'leaf_mass': [self.plantFATE_model.props.leaf_mass],
+            'resp_auto': [self.plantFATE_model.props.resp_auto],
+            'stem_mass': [self.plantFATE_model.props.stem_mass]
+        })
+        self.emergentProps = pd.concat([self.emergentProps, e])
+
+    def exportEnvironment(self, out_file):
+        self.environment.to_csv(out_file, sep=',', index=False, encoding='utf-8')
+
+    def exportEmergentProps(self, out_file):
+        self.emergentProps.to_csv(out_file, sep=',', index=False, encoding='utf-8')
+
+    def exportSpeciesProps(self, out_file):
+        self.speciesProps.to_csv(out_file, sep=',', index=False, encoding='utf-8')
 
 class PlantFATECoupling:
     def __init__(self, param_file):
-        self.plantFATE_model = pyplantFATE.PlantFATERunner(param_file)
+        self.plantFATE_model = PlantFATERunner(param_file)
 
     @property
     def n_individuals(self):
